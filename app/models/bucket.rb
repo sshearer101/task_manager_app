0f4cc32c6b34ab606421a25bc9841a07aa1f1a02
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-11-19 11:56:13 -0700

    first

---
+++ app/models/bucket.rb
@@ -0,0 +1,27 @@
+
+class Bucket < ApplicationRecord
+    has_many :tasks
+
+    def self.get_by_status(status)
+        self.all.select do |bucket|
+            bucket.status == status
+        end
+    end
+
+    def update_status
+        if self.tasks == []
+            self.status = "Empty"
+        elsif self.tasks.any?{|task| task.status == "Pending"}
+            self.status = "Pending"
+        else
+            self.status = "Completed"
+        end
+        self.save
+    end
+
+    def tasks_by_status(status)
+        self.tasks.select do |task|
+            task.status == status
+        end
+    end
+end
